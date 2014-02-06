81512b4feb5d6a2f3c6e45e7f529779e4a3d03c4
bchhun <bernard.chhun@gmail.com>
2014-02-06 14:50:35 -0500

    ajout de commentaires

--- src/csv_to_db_example.py
+++ src/csv_to_db_example.py
@@ -8,18 +8,22 @@ from datetime import datetime
 from db import User, connect, init_db
 
 CSV_PATH = os.path.join("..", "data", "*.csv")
-CSV_FILES = glob(CSV_PATH)
-CSV_FILES.reverse()
-
+csv_files = glob(CSV_PATH)
+csv_files.reverse()
 
 if __name__ == '__main__':
     init_db()
     session = connect(debug=True)
 
-    for csv_file in CSV_FILES:
+    # Pour chacun des fichiers csv dans le dossier data
+    for csv_file in csv_files:
+        # ouverture du fichier
         with open(csv_file) as f:
+            # utilisation de DictReader pour transformer chaque ligne
+            # du fichier texte en tableau avec des dictionnaires
             csv_content = csv.DictReader(f)
 
+            # instanciation des users pour chaque ligne du DictReaders
             session.add_all((
                 User(
                     nas=row["nas"],
